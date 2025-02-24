b57e6862aa5ebbf1bbe68e755acb63046d6fc4ce
alfred <apolinarioalfred004@gmail.com>
2025-02-24 18:06:58 +0100

    esercizio

---
+++ esercizio.js
@@ -0,0 +1,23 @@
+let nomeStudente = "Alfred";
+let etaStudente = 20;
+let corsoAttivo = true; // Booleano
+
+console.log("Il nome dello studente è:",nomeStudente);
+console.log("L'età dello studente è:",etaStudente);
+console.log("Lo studente è iscritto al corso?", corsoAttivo);
+
+let numero1 = 10;
+let numero2 = 3;
+let risultatoSomma = numero1 + numero2;
+let risultatoSottrazione = numero1 - numero2;
+let risultatoMoltiplicazione = numero1 + numero2;
+let risultatoDivisione = numero1 / numero2;
+
+console.log("Somma:", risultatoSomma);
+console.log("Sottrazione:", risultatoSottrazione);
+console.log("Moltiplicazione:", risultatoMoltiplicazione);
+console.log("Divisione:", risultatoDivisione);
+
+let primaParte = "Ciao, io sono";
+let secondaParte = "e ho";
+let terzaParte = " anni";
